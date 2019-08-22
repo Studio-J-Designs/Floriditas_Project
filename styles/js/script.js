392d98f007c117af05899c52ed0964d9a26283cf
Studio J Designs <53199757+Studio-J-Designs@users.noreply.github.com>
2019-08-22 15:20:30 +1200

    General
    Added a whole buch of style and other assets

---
+++ styles/js/script.js
@@ -0,0 +1,12 @@
+console.log('hi')
+
+// $('nav').hide();
+
+//click even for hambuger
+$('.myIcon').click(function(){
+	$('nav').toggle();
+})
+
+$('.myIcon2').click(function(){
+	$('nav').toggle();
+})
